085c4ef3c7165c8886a0730d38f8dc5439f2b6fb
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-03-31 23:15:01 +0800

    Update

--- node.js
+++ node.js
@@ -2,16 +2,16 @@ const textArea = document.getElementById('textArea');
 const character = document.getElementById('char');
 const words = document.getElementById('word');
 
-textArea.addEventListener('inout', function() {
+textArea.addEventListener('input', function() {
     let text = this.value;
     text = text.trim();
     let char = text.length;
     character.value = char;
+
     let newText = text.split(" ");
     let cleanWord = newText.filter((element) => {
         return element != "";
     });
 
-    words.value = cleanWord.lenght
-
+    words.value = cleanWord.length;
 });
