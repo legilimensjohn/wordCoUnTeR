f7a22e7752d7872576892510dbc61e832b7c3544
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-06-25 23:14:23 +0800

    Update node.js

--- node.js
+++ node.js
@@ -5,17 +5,22 @@ const words = document.getElementById('word');
 
 // Add an event listener to the textarea for input events
 textArea.addEventListener('input', function() {
+
     // Retrieve the text from the textarea
     let text = this.value;
+
     // Trim the text to remove leading and trailing whitespace
     text = text.trim();
+
     // Calculate the number of characters in the text
     let char = text.length;
+
     // Update the character count in the corresponding input field
     character.value = char;
 
     // Split the text into an array of words
     let newText = text.split(" ");
+
     // Filter out empty strings (occurs if there are multiple spaces)
     let cleanWord = newText.filter((element) => {
         return element != "";
